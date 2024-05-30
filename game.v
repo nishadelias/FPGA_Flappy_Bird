27bc6ff6a4db4d63696696ae9dcf111552e4b04c
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-30 11:53:55 -0700

    Update game.v

--- game.v
+++ game.v
@@ -3,40 +3,46 @@ module game(
   input flap,
   input pause,
   input reset,
-  output game_state,
+  output wire game_state,
   output wire [9:0] current_score,
   output wire [9:0] highest_score,
-  output wire [7:0] bird_x,
+  output wire [9:0] bird_x,
   output wire [8:0] bird_y
 );
+
+reg gs;
+reg [9:0] curr_score;
+reg[9:0] high_score;
+reg[9:0] x;
+reg[8:0] y;
 
   reg [6:0] velocity;
   reg positive;    // keep track of whether the velocity is positive or negative
 
   initial begin
-    game_state <= 0;
-    current score <= 0;
-    highest score <= 0;
-    bird_x <= 140;
-    bird_y <= 280;
+    gs <= 0;
+    curr_score <= 0;
+    high_score <= 0;
+    x <= 140;
+    y <= 280;
     velocity <= 0;
     positive <= 1;
   end
 
   always @(*) begin
     if (reset) begin
-      game_state <= 0;
-      current score <= 0;
-      highest score <= 0;
-      bird_x <= 140;
-      bird_y <= 280;
+      gs <= 0;
+      curr_score <= 0;
+      high_score <= 0;
+      x <= 140;
+      y <= 280;
       velocity <= 0;
       positive <= 1;
     end else if (!pause) begin
-      if (!game_state && flap) begin
-        game_state <= 1;
+      if (!gs && flap) begin
+        gs <= 1;
         velocity <= 10;
-      end else if (game_state && !flap) begin
+      end else if (gs && !flap) begin
         if (velocity == 0) begin
           positive <= 0;
           velocity <= velocity + 1;
@@ -48,16 +54,23 @@ module game(
         positive <= 1;
       end
 
-      if (game_state) begin
-        if (positive) begin
-          bird_y <= bird_y + velocity;
-        end else begin
-          bird_y <= bird_y - velocity;
+      if (gs) begin
+        if (positive && y < 465) begin
+          y <= y + velocity;
+        end else if (!positive && y > 40) begin
+          y <= y - velocity;
         end
       end
     end
-
+   
+    
 
   end
+  
+  assign game_state = gs;
+  assign current_score = curr_score;
+  assign highest_score = high_score;
+  assign bird_x = x;
+  assign bird_y = y;
 
 endmodule
